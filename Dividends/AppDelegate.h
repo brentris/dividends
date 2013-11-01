b77ede5c6488a6aa604938fe54534a599b2fa6cc
Brent Rissman <brentris@iMac.local>
2013-11-01 01:52:39 -0700

    Initial Commit

---
+++ Dividends/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dividends
+//
+//  Created by Brent Rissman on 11/1/13.
+//  Copyright (c) 2013 Brent Rissman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dividends/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dividends
+//
+//  Created by Brent Rissman on 11/1/13.
+//  Copyright (c) 2013 Brent Rissman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
